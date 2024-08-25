1176d51f980d4c5ffb9482f2bba89e86db81cc31
“ireshan” <ireshandj2@gmail.com>
2024-08-25 11:10:02 +0800

    My-hrm: user CRUD operatons template

---
+++ src/user/user.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Version } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Prisma } from '@prisma/client';
+
+
+@Controller('user')
+export class UserController {
+  
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  @Version('1')
+  create(@Body() createUserDto: Prisma.UserCreateInput) {
+    console.log("this is test log");
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  @Version('1')
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  @Version('1')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @Version('1')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  @Version('1')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
