efdb53d3d854c4c3cb1e2314ac6418595ac637cb
“ireshan” <ireshandj2@gmail.com>
2024-09-07 00:00:13 +0800

    My-hrm: add response transform interceptor

---
+++ src/interceptor/responseTransform.interceptor.ts
@@ -0,0 +1,35 @@
+/**
+ * Generic response transformer interceptor
+ * tranform the response
+ * log the response
+ */
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(context: ExecutionContext, next: CallHandler<T>): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => {
+        const ctx = context.switchToHttp();
+        const response = ctx.getResponse();
+        // const request = ctx.getRequest();
+
+        const statusCode = response.statusCode;
+        // const url = request.originalUrl;
+        const res = {
+          statusCode,
+          msg: null,
+          success: true,
+          data,
+        };
+        return res;
+      })
+    );
+  }
+}
