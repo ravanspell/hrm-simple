451a3b823a5b1f7e08fe58eba0efc860fdb0fcda
“ireshan” <ireshandj2@gmail.com>
2024-09-08 14:30:09 +0800

    My-hrm: log db quries and errors - prisma settings

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -3,6 +3,12 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        super({
+            log: ['query', 'info', 'warn', 'error'], // Enable query logging and other log levels
+        });
+    }
+
     async onModuleInit() {
         await this.$connect();
     }
