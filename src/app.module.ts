3a5561aa3d7bf3c3955027e5b2ffb1480d70bf39
“ireshan” <ireshandj2@gmail.com>
2024-09-19 22:50:17 +0800

    My-hrm: JWT auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,16 @@ import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { LoggerModule } from './logger/logger.module';
 import { EmployeeLeavesModule } from './employee-leaves/employee-leaves.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({isGlobal: true}),
     DatabaseModule, 
     UserModule, 
     LoggerModule, 
-    EmployeeLeavesModule
+    EmployeeLeavesModule, 
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService
+    ) { }
+
+    async login(user: Prisma.UserCreateInput, response: Response) {
+        const jwtExpireTime = new Date();
+        jwtExpireTime.setMilliseconds(
+            jwtExpireTime.getTime() + parseInt(process.env.JWT_ACCESS_TOKEN_EXPIRATION_MS)
+        );
+
+        const jwtPayload = {
+            userId: user.email,
+        };
+        const jwtToken = this.jwtService.sign(jwtPayload);
+        console.log("user jwt", jwtToken);
+        response.cookie('accessToken', jwtToken, {
+            httpOnly: true,
+            secure: false,
+            expires: jwtExpireTime,
+        })
+        
+    }
+
+    async verifyUser(email: string, password: string) {
+        const user = await this.userService.findOne(email);
+        if (!user) {
+            return null;
+        }
+        const authenticated = await await compare(password, user.password);
+        if (!authenticated) {
+            return null;
+        }
+        return user;
+    }
+}
