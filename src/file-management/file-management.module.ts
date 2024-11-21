13e4138d1f94d933d59b4abc25ff68d1c7c2ba36
“ireshan” <ireshandj2@gmail.com>
2024-11-21 23:03:08 +0800

    my hrm: add aws s3 service into file mgt module

--- src/file-management/file-management.module.ts
+++ src/file-management/file-management.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FileManagementService } from './file-management.service';
 import { FileManagementController } from './file-management.controller';
+import { AwsS3Service } from './aws-S3.service';
 
 @Module({
   controllers: [FileManagementController],
-  providers: [FileManagementService],
+  providers: [
+    FileManagementService,
+    AwsS3Service,
+  ],
 })
 export class FileManagementModule {}
