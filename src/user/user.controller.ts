c22c14b903c225c7eeee8f1483e24d1f8af45c0b
“ireshan” <ireshandj2@gmail.com>
2024-12-05 08:38:32 +0800

    my hrm: add role create and update endpoints as controller methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Version, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Version, Req, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -7,20 +7,24 @@ import { FilterUsersDto } from './dto/filter-user.dto';
 import { Permissions } from 'src/decorators/permissions.decorator';
 import { Authentication } from 'src/decorators/auth.decorator';
 import { RequestWithTenant } from 'src/coretypes';
+import { RolesService } from './roles.service';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { UpdateRoleDto } from './dto/update-role.dto';
 
 
 @Controller('user')
 export class UserController {
-  
+
   constructor(
     private readonly userService: UserService,
-    private readonly loggerService: LoggerService
-  ) {}
+    private readonly loggerService: LoggerService,
+    private readonly roleService: RolesService
+  ) { }
 
   @Post()
   @Version('1')
   create(@Body() createUserDto: CreateUserDto) {
-    this.loggerService.logEmployeeAction('user create req',JSON.stringify(createUserDto));
+    this.loggerService.logEmployeeAction('user create req', JSON.stringify(createUserDto));
     return this.userService.create(createUserDto);
   }
 
@@ -29,7 +33,7 @@ export class UserController {
   @Authentication()
   @Permissions('can:filter')
   filter(@Body() filterUsersDto: FilterUsersDto[], @Req() request: RequestWithTenant) {
-    this.loggerService.logEmployeeAction('im first log here','emp id');
+    this.loggerService.logEmployeeAction('im first log here', 'emp id');
     const orgId = request.user.organizationId;
     return this.userService.filterUsers(filterUsersDto, orgId);
   }
@@ -50,5 +54,19 @@ export class UserController {
   @Version('1')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
+  }
+
+  // ----------- roles ----------------
+
+  @Post('role')
+  @Version('1')
+  createRole(@Body() createRoleData: CreateRoleDto) {
+    return this.roleService.createRole(createRoleData);
+  }
+
+  @Put('role/:id')
+  @Version('1')
+  updateRole(@Param('id') id: string, @Body() updateRoleData: UpdateRoleDto) {
+    return this.roleService.updateRole(id, updateRoleData);
   }
 }
