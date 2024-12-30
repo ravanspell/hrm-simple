a4c1fb64fc6c582abb9a8e007b47ad1be167e90b
“ireshan” <ireshandj2@gmail.com>
2024-12-30 12:50:24 +0800

    change auth session store into type ORM session store configs

---
+++ src/constants/dbTables.ts
@@ -0,0 +1,18 @@
+
+export const SESSION_TABLE = 'Session';
+export const USER_TABLE = 'User';
+export const ORGANIZATION_TABLE = 'Organization';
+export const EMPLOYMENT_STATUS_TABLE = 'EmploymentStatus';
+export const EMPLOYEE_LEVEL_TABLE = 'EmployeeLevel';
+export const EMPLOYEE_TABLE = 'Employee';
+export const EMPLOYEE_HISTORY_TABLE = 'EmployeeHistory';
+export const EMPLOYEE_STATUS_TABLE = 'EmployeeStatus';
+export const EMPLOYEE_STATUS_HISTORY_TABLE = 'EmployeeStatusHistory';
+export const EMPLOYEE_LEVEL_HISTORY_TABLE = 'EmployeeLevelHistory';
+export const EMPLOYMENT_STATUS_HISTORY_TABLE = 'EmploymentStatusHistory';
+export const ORGANIZATION_HISTORY_TABLE = 'OrganizationHistory';
+export const USER_HISTORY_TABLE = 'UserHistory';
+export const SESSION_HISTORY_TABLE = 'SessionHistory';
+export const EMPLOYEE_STATUS = 'EmployeeStatus';
+export const EMPLOYEE_LEVEL = 'EmployeeLevel';
+export const EMPLOYMENT_STATUS = 'EmploymentStatus';
