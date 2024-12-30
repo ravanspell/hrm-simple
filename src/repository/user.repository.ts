bf8f9891b2b55290242c1a11cd394dfa398880eb
“ireshan” <ireshandj2@gmail.com>
2024-12-30 14:29:20 +0800

    my hrm: add block docs

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,3 +1,8 @@
+/**
+ * UserRepository is a custom repository for the User entity.
+ * It extends the TypeORM Repository class and provides additional
+ * methods for querying the User entity.
+ */
 import { User } from 'src/user/entities/user.entity';
 import { Repository, DataSource } from 'typeorm';
 
@@ -7,12 +12,21 @@ export class UserRepository extends Repository<User> {
     constructor(dataSource: DataSource) {
         super(User, dataSource.createEntityManager());
     }
-
+    /**
+     * Finds a user by their email address.
+     * @param email - The email address of the user to find.
+     * @returns A promise that resolves to the found User entity.
+     */
     findUser(email: string): Promise<User> {
         return this.findOne({ where: { email }, relations: ['organization'] });
     }
 }
 
+/**
+ * UserRepositoryProvider is a provider for the UserRepository.
+ * It uses a factory function to create an instance of UserRepository
+ * and injects the DataSource dependency.
+ */
 export const UserRepositoryProvider = {
     provide: USER_REPOSITORY,
     useFactory: (dataSource: DataSource) => new UserRepository(dataSource),
