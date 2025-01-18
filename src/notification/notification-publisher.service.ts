07f2fc50e5f891a6d3785ff2bb4e2196c853305a
“ireshan” <ireshandj2@gmail.com>
2025-01-18 10:29:34 +0800

    my hrm: notification publisher service

---
+++ src/notification/notification-publisher.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@nestjs/common';
+import { AwsSqsService } from '../utilities/aws-sqs-service/aws-sqs.service';
+import { ConfigService } from '@nestjs/config';
+import { v4 as uuidv4 } from 'uuid';
+
+type NotificationTypePayloads = {
+    email: {
+        userId: string;
+        subject?: string;
+        body?: string;
+        templateId?: string;
+    };
+    webPush: {
+        userId: string;
+        subject: string;
+        body: string;
+    };
+};
+
+@Injectable()
+export class NotificationPublisherService {
+    private readonly notificationQueueUrl: string;
+
+    constructor(
+        private readonly awsSqsService: AwsSqsService,
+        private readonly configService: ConfigService,
+    ) {
+        this.notificationQueueUrl = this.configService.get<string>(
+            'NOTIFICATION_QUEUE_URL',
+        );
+    }
+
+    /**
+     * Publish a notification message.
+     *
+     * This wrapper method centralizes the logic for publishing messages to the SQS queue.
+     * It ensures that other modules don't need direct access to AwsSqsService, providing better encapsulation.
+     *
+     * @param type - The type of notification (e.g., 'email', 'notification', etc.).
+     * @param payload - The message content to be sent.
+     *
+     * @example
+     * await notificationsService.publishNotification('email', { subject: 'Hello', body: 'World' });
+     */
+
+    async publishNotification<T extends keyof NotificationTypePayloads>(
+        type: T,
+        payload: NotificationTypePayloads[T],
+    ): Promise<void> {
+        const messageBody = JSON.stringify({ type, payload });
+
+        await this.awsSqsService.publishMessage(
+            this.notificationQueueUrl,
+            messageBody,
+            { Type: type, id: uuidv4() }, // Add attributes for filtering or categorization
+        );
+        console.log(`Notification of type "${type}" published to the queue.`);
+    }
+}
