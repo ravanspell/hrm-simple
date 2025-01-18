021ac9b52ebb210fbad914ee94f0e2e74b79528f
“ireshan” <ireshandj2@gmail.com>
2025-01-18 10:35:46 +0800

    my hrm: register notification strategies with notification consumer service

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,12 +1,30 @@
-import { Module } from '@nestjs/common';
-import { NotificationService } from './notification.service';
-import { NotificationFactory } from './notification.factory';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { EmailSettingsModule } from 'src/email-settings/email-settings.module';
 import { AwsSqsService } from '../utilities/aws-sqs-service/aws-sqs.service';
+import { EmailStrategy } from './strategies/email/email.strategy';
+import { WebPushNotificationStrategy } from './strategies/webPushNotification/webPushNotification.strategy';
+import { NotificationConsumerService } from './notification-consumer.service';
+import { NotificationPublisherService } from './notification-publisher.service';
+import { SessionService } from '@/auth/session.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Session } from '@/auth/entities/session.entity';
 
 @Module({
-  imports: [EmailSettingsModule],
-  providers: [AwsSqsService, NotificationService, NotificationFactory],
-  exports: [NotificationService],
+  imports: [EmailSettingsModule, TypeOrmModule.forFeature([Session])],
+  providers: [AwsSqsService, NotificationPublisherService, NotificationConsumerService, EmailStrategy, WebPushNotificationStrategy, SessionService],
+  exports: [NotificationPublisherService],
 })
-export class NotificationModule {}
+
+export class NotificationModule implements OnModuleInit {
+
+  constructor(
+    private readonly notificationConsumerService: NotificationConsumerService,
+    private readonly emailStrategy: EmailStrategy,
+    private readonly webPushStrategy: WebPushNotificationStrategy,
+  ) {}
+
+  onModuleInit() { 
+    this.notificationConsumerService.registerStrategy(this.emailStrategy);
+    this.notificationConsumerService.registerStrategy(this.webPushStrategy);
+  }
+}
