8151538514251e5018a4536777d7c0aba7ebd026
“ireshan” <ireshandj2@gmail.com>
2025-02-09 23:55:32 +0800

    my hrm: networking cdktf upgrade

--- my-hrm-infra/aws/stacks/core/networking.ts
+++ my-hrm-infra/aws/stacks/core/networking.ts
@@ -1,12 +1,14 @@
-import { TerraformOutput, TerraformStack } from "cdktf";
+import { TerraformOutput } from "cdktf";
 import { Construct } from "constructs";
 import { Vpc } from "@cdktf/provider-aws/lib/vpc";
 import { Subnet, SubnetConfig } from "@cdktf/provider-aws/lib/subnet";
 import { InternetGateway } from "@cdktf/provider-aws/lib/internet-gateway";
 import { RouteTable } from "@cdktf/provider-aws/lib/route-table";
 import { Route } from "@cdktf/provider-aws/lib/route";
+import { PUBLIC_SUBNET_ID_KEY, VPC_ID_KEY } from "../../constants";
+import { RouteTableAssociation } from "@cdktf/provider-aws/lib/route-table-association";
 
-export class NetworkingStack extends TerraformStack {
+export class NetworkingStack extends Construct {
     public readonly vpc: Vpc;
     public readonly internetGateway: InternetGateway;
     public readonly publicSubnets: Subnet[];
@@ -71,7 +73,15 @@ export class NetworkingStack extends TerraformStack {
             });
         });
 
-        new TerraformOutput(this, "vpc-id", { value: this.vpc.id });
-        new TerraformOutput(this, "public-subnet-id", { value: this.publicSubnets[0].id });
+        // Create a route‐table association for the public subnet
+        new RouteTableAssociation(this, "public-subnet-assoc", {
+            subnetId: this.publicSubnets[0].id,
+            routeTableId: publicRouteTable.id,
+        });
+
+        console.log("this.vpc.id xxx--->", this.vpc.id);
+
+        new TerraformOutput(this, VPC_ID_KEY, { value: this.vpc.id });
+        new TerraformOutput(this, PUBLIC_SUBNET_ID_KEY, { value: this.publicSubnets[0].id });
     }
 }
