d6acad774d84ea60429be2c4b7938ae4b73d02d6
“ireshan” <ireshandj2@gmail.com>
2025-02-11 23:57:10 +0800

    my hrm: refactor user data script of the EC2 instance

--- my-hrm-infra/aws/stacks/compute/user-data.sh
+++ my-hrm-infra/aws/stacks/compute/user-data.sh
@@ -22,11 +22,85 @@ exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&
 
 echo "Starting user data script execution..."
 
+# change execution dir into ec2 user dir
+cd /home/ec2-user/
+
 # Update package repository and upgrade system packages
 sudo dnf update -y
 
 # Install necessary utilities
 sudo dnf install -y unzip git
+
+# install NVM - node version manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+#Load nvm
+source ~/.bashrc
+
+# install node.js version 20.17.0 as default with NVM
+nvm install default 20.17.0
+
+#select installed node.js version
+nvm use 20.17.0
+
+# Install PM2 globally
+sudo npm install -g pm2
+
+# Ensure PM2 starts on boot for ec2-user
+pm2 startup systemd -u ec2-user --hp /home/ec2-user
+sudo env PATH=$PATH:/home/ec2-user/.nvm/versions/node/v22.13.1/bin pm2 startup systemd -u ec2-user --hp /home/ec2-user
+
+# Create a dedicated directory for the backend
+mkdir -p backend
+cd backend
+
+# Change ownership to ec2-user
+sudo chown -R ec2-user:ec2-user backend
+
+# Initialize Git repo (if not already initialized)
+if [ ! -d ".git" ]; then
+  echo "Initializing Git repository..."
+  git init
+  git remote add origin https://github.com/ravanspell/hrm-simple.git
+fi
+
+# Create a new repository file for MariaDB
+sudo tee /etc/yum.repos.d/mariadb.repo << EOF
+[mariadb]
+name = MariaDB
+baseurl = https://mirror.rackspace.com/mariadb/yum/11.2/rhel9-amd64/
+gpgkey = https://mirror.rackspace.com/mariadb/yum/RPM-GPG-KEY-MariaDB
+gpgcheck = 1
+enabled = 1
+EOF
+
+# Clean and update DNF cache
+sudo dnf clean all
+sudo dnf makecache
+
+# Now install MariaDB
+sudo dnf install -y MariaDB-server MariaDB-client
+
+# Start MariaDB
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+while ! mariadb-admin ping -h localhost --silent; do 
+  echo "Waiting for MariaDB to be ready..." 
+  sleep 2 
+done
+
+# Fetch password from AWS Secrets Manager
+DB_PASSWORD=$(aws ssm get-parameter \
+    --name "/hrm-app/PRIMARY_DATABASE_PASSWORD" \
+    --with-decryption \
+    --query 'Parameter.Value' \
+    --output text)
+
+#login to mariadb default root user
+sudo mariadb -u root <<EOF
+CREATE DATABASE IF NOT EXISTS myhrm_dev;
+EOF
 
 # Install AWS CloudWatch Agent
 sudo dnf install -y amazon-cloudwatch-agent
@@ -35,7 +109,7 @@ sudo dnf install -y amazon-cloudwatch-agent
 mkdir -p /opt/aws/amazon-cloudwatch-agent/etc/
 
 # Cloudwatch agent config for pick Ec2 instance data and application logs
-cat > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json << 'EOF'
+sudo cat > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json << 'EOF'
 {
   "agent": {
     "metrics_collection_interval": 60
@@ -74,91 +148,5 @@ EOF
 # Start and enable the agent
 systemctl start amazon-cloudwatch-agent
 systemctl enable amazon-cloudwatch-agent
-
-# Create a new repository file for MariaDB
-sudo tee /etc/yum.repos.d/mariadb.repo << EOF
-[mariadb]
-name = MariaDB
-baseurl = https://mirror.rackspace.com/mariadb/yum/11.2/rhel9-amd64/
-gpgkey = https://mirror.rackspace.com/mariadb/yum/RPM-GPG-KEY-MariaDB
-gpgcheck = 1
-enabled = 1
-EOF
-
-# Clean and update DNF cache
-sudo dnf clean all
-sudo dnf makecache
-
-# Now install MariaDB
-sudo dnf install -y MariaDB-server MariaDB-client
-
-# Start MariaDB
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
-
-while ! mariadb-admin ping -h localhost --silent; do 
-  echo "Waiting for MariaDB to be ready..." 
-  sleep 2 
-done
-
-# Fetch password from AWS Secrets Manager
-DB_PASSWORD=$(aws ssm get-parameter \
-    --name "/hrm-app/PRIMARY_DATABASE_PASSWORD" \
-    --with-decryption \
-    --query 'Parameter.Value' \
-    --output text)
-
-#login to mariadb default root user
-sudo mariadb -u root <<EOF
-
-# Set root password
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';
-
-# Create application database
-CREATE DATABASE IF NOT EXISTS myhrm_dev;
-
-# Create application database
-CREATE DATABASE IF NOT EXISTS myhrm_dev;
-
-# Create application user with limited privileges
-CREATE USER 'myhrm_user'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON myhrm_dev.* TO 'myhrm_user'@'localhost';
-
-# Apply changes
-FLUSH PRIVILEGES;
-EOF
-
-# install NVM - node version manager
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-
-#Load nvm
-source ~/.bashrc
-
-# install node.js version 20.17.0 as default with NVM
-nvm install default 20.17.0
-
-#select installed node.js version
-nvm use 20.17.0
-
-# Install PM2 globally
-sudo npm install -g pm2
-
-# Ensure PM2 starts on boot for ec2-user
-pm2 startup systemd -u ec2-user --hp /home/ec2-user
-sudo env PATH=$PATH:/home/ec2-user/.nvm/versions/node/v22.13.1/bin pm2 startup systemd -u ec2-user --hp /home/ec2-user
-
-# Create a dedicated directory for the backend
-mkdir -p backend
-cd backend
-
-# Change ownership to ec2-user
-sudo chown -R ec2-user:ec2-user backend
-
-# Initialize Git repo (if not already initialized)
-if [ ! -d ".git" ]; then
-  echo "Initializing Git repository..."
-  git init
-  git remote add origin https://github.com/ravanspell/hrm-simple.git
-fi
 
 echo "EC2 Instance is fully configured for API deployment!"
