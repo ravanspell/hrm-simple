c7a728f715b9e2097ac8ad374945ac76ac6e431d
“ireshan” <ireshandj2@gmail.com>
2025-02-15 10:42:54 +0800

    my hrm: forget password request DTO

--- src/auth/dto/forget-password.dto.ts
+++ src/auth/dto/forget-password.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class ForgetPasswordRequest {
-    @ApiProperty({
-        description: 'Reset token',
-        example: 'some-random-token',
-    })
-    @IsNotEmpty({message: 'Email required'})
-    @IsEmail()
-    email: string;
+  @ApiProperty({
+    description: 'Reset token',
+    example: 'some-random-token',
+  })
+  @IsNotEmpty({ message: 'Email required' })
+  @IsEmail()
+  email: string;
 }
