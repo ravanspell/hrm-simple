156bf71848458c7031adf74763a3bfdaa79f4829
“ireshan” <ireshandj2@gmail.com>
2025-04-12 22:45:50 +0800

    my hrm: when file upload we change the upload value.

--- src/file-management/file-management.module.ts
+++ src/file-management/file-management.module.ts
@@ -4,8 +4,10 @@ import { FileManagementController } from './file-management.controller';
 import { AwsS3Service } from '../utilities/aws-s3-service/aws-S3.service';
 import { FileMgtRepository } from 'src/repository/file-management.repository';
 import { FolderRepository } from 'src/repository/folder.repository';
+import { OrganizationModule } from '../organization/organization.module';
 
 @Module({
+  imports: [OrganizationModule],
   controllers: [FileManagementController],
   providers: [
     FileManagementService,
