6af2af54e8e529299325931c6d430378a67f14d8
“ireshan” <ireshandj2@gmail.com>
2025-04-20 23:05:14 +0800

    my hrm: move create system permision API into system permission controller

--- src/permission/permission.controller.ts
+++ src/permission/permission.controller.ts
@@ -4,19 +4,16 @@ import {
   Get,
   HttpStatus,
   Param,
-  Post,
   Put,
   Version,
 } from '@nestjs/common';
 import { PermissionService } from './permission.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { CreateSystemPermissionDto } from './dto/create-system-permission.dto';
 import { Authentication } from '@/decorators/auth.decorator';
 import { API_VERSION } from '@/constants/common';
 import { UpdateSystemPermissionDto } from './dto/update-system-permission.dto';
 import { CurrentUser } from '@/decorators/current-user.decorator';
 import { User } from '@/user/entities/user.entity';
-import { SystemPermission } from './entities/system-permission.entity';
 
 @ApiTags('User Permissions')
 @Controller('permission')
@@ -24,7 +21,7 @@ import { SystemPermission } from './entities/system-permission.entity';
 export class PermissionController {
   constructor(private readonly permissionService: PermissionService) {}
 
-  @Get('user-permissions')
+  @Get()
   @Version(API_VERSION.V1)
   @ApiOperation({ summary: 'Get system permissions with filters' })
   @ApiResponse({
@@ -34,40 +31,6 @@ export class PermissionController {
   async getPermissions(@CurrentUser() user: User) {
     const userId = user.id;
     return this.permissionService.getUserEffectivePermissions(userId);
-  }
-
-  /**
-   * Create a new system permission.
-   *
-   * @param createSystemPermissionDto - Data transfer object containing permission details
-   * @param _user - Current authenticated user
-   * @returns The created permission
-   * @throws ConflictException if permission with same key exists
-   * @throws BadRequestException if category not found
-   */
-  @Post()
-  @Version(API_VERSION.V1)
-  @ApiOperation({ summary: 'Create a new system permission' })
-  @ApiResponse({
-    status: HttpStatus.CREATED,
-    description: 'The system permission has been successfully created.',
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'Invalid input data or category not found.',
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'Permission with this key already exists.',
-  })
-  async createSystemPermission(
-    @Body() createSystemPermissionDto: CreateSystemPermissionDto,
-    @CurrentUser() user: User,
-  ): Promise<SystemPermission> {
-    return this.permissionService.createSystemPermission(
-      createSystemPermissionDto,
-      user.id,
-    );
   }
 
   /**
