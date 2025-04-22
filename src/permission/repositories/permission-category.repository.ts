48d934a5e5d6077b9e69ba95503d3e45b45ce4de
“ireshan” <ireshandj2@gmail.com>
2025-04-22 23:22:32 +0800

    my hrm: create permission upsert repo method

--- src/permission/repositories/permission-category.repository.ts
+++ src/permission/repositories/permission-category.repository.ts
@@ -9,6 +9,27 @@ export class PermissionCategoryRepository extends Repository<PermissionCategory>
   }
 
   /**
+   * Create or update a permission category
+   * If a category with the same key exists, it will be updated
+   * If no category exists with the key, a new one will be created
+   * @param categoryData Partial<PermissionCategory> data to create or update
+   * @returns Created or updated category
+   */
+  async upsertCategory(
+    categoryData: Partial<PermissionCategory>,
+  ): Promise<PermissionCategory> {
+    const result = await this.createQueryBuilder()
+      .insert()
+      .into(PermissionCategory)
+      .values(categoryData)
+      .orUpdate(['name', 'description', 'displayOrder', 'updatedBy'], ['key'])
+      .returning('*')
+      .execute();
+
+    return result.raw[0];
+  }
+
+  /**
    * Find permission category with category id
    * @param id Category ID
    * @returns PermissionCategory
@@ -27,6 +48,17 @@ export class PermissionCategoryRepository extends Repository<PermissionCategory>
   async findByName(name: string): Promise<PermissionCategory> {
     return this.findOne({
       where: { name },
+    });
+  }
+
+  /**
+   * Find category by key
+   * @param key Category key
+   * @returns PermissionCategory
+   */
+  async findByKey(key: string): Promise<PermissionCategory> {
+    return this.findOne({
+      where: { key },
     });
   }
 }
