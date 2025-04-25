c8cd641533d4c08e5fff7b23ed50db696b330d54
“ireshan” <ireshandj2@gmail.com>
2025-04-25 22:12:18 +0800

    my hrm: minor changes

--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -3,9 +3,15 @@ import { RoleService } from './role.service';
 import { RoleController } from './role.controller';
 import { RoleRepository } from './repository/role.repository';
 import { UserRoleRepository } from './repository/user-role.repository';
+import { RolePermissionRepository } from './repository/role-permission.repository';
 
 @Module({
   controllers: [RoleController],
-  providers: [RoleService, RoleRepository, UserRoleRepository],
+  providers: [
+    RoleService,
+    RoleRepository,
+    UserRoleRepository,
+    RolePermissionRepository,
+  ],
 })
 export class RoleModule {}
