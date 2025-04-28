03e3e7c73420c36177580ed611d1be184a522bb1
“ireshan” <ireshandj2@gmail.com>
2025-04-29 00:31:32 +0800

    my hrm: user audit db subscriber

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -13,3 +13,14 @@ export const ENVIRONMENT = {
   LOCAL: 'local',
   DEVELOPMENT: 'dev',
 } as const;
+
+export const BASE_ENTITY_COLUMNS = {
+  ID: 'id',
+  CREATED_AT: 'createdAt',
+  UPDATED_AT: 'updatedAt',
+  CREATED_BY: 'createdBy',
+  UPDATED_BY: 'updatedBy',
+} as const;
+
+// context keys
+export const USER_CONTEXT_KEY = 'currentUser';
---
+++ src/database/subscribers/audit-metadata.subscriber.ts
@@ -0,0 +1,54 @@
+import {
+  EventSubscriber,
+  EntitySubscriberInterface,
+  InsertEvent,
+  UpdateEvent,
+} from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { AsyncStorageService } from '@/utilities/async-storage-service/async-storage.service';
+import { USER_CONTEXT_KEY } from '@/constants/common';
+import { User } from '@/user/entities/user.entity';
+import { BaseEntity } from '@/common/entities/base.entity';
+
+@Injectable()
+@EventSubscriber()
+export class AuditMetadataSubscriber implements EntitySubscriberInterface {
+  private readonly asyncStorageService = AsyncStorageService.getInstance();
+
+  // set the createdBy field automatically
+  setCreatedBy(entity: any, currentUserId: string) {
+    if (entity instanceof BaseEntity) {
+      entity.createdBy = currentUserId;
+    }
+  }
+
+  // set the updatedBy field automatically
+  setUpdatedBy(entity: any, currentUserId: string) {
+    if (entity instanceof BaseEntity) {
+      entity.updatedBy = currentUserId;
+    }
+  }
+
+  /**
+   * Set the createdBy and updatedBy fields automatically
+   * @param event
+   */
+  beforeInsert(event: InsertEvent<any>) {
+    const currentUser = this.asyncStorageService.get<User>(USER_CONTEXT_KEY);
+    if (currentUser?.id) {
+      this.setCreatedBy(event.entity, currentUser.id);
+      this.setUpdatedBy(event.entity, currentUser.id);
+    }
+  }
+
+  /**
+   * Set the updatedBy field automatically
+   * @param event
+   */
+  beforeUpdate(event: UpdateEvent<any>) {
+    const currentUser = this.asyncStorageService.get<User>(USER_CONTEXT_KEY);
+    if (currentUser?.id) {
+      this.setUpdatedBy(event.entity, currentUser.id);
+    }
+  }
+}
